b0ef594004b79f295ad00bf7b7428ac7fc8b1bbd
Siddharth Singh <144914078+SiddharthSingh00769@users.noreply.github.com>
2025-08-30 16:26:48 +0530

    Add files via upload

---
+++ bms/src/bank/management/system/Con.java
@@ -0,0 +1,18 @@
+package bank.management.system;
+
+import java.sql.*;
+
+public class Con {
+    Connection connection;
+    Statement statement;
+    public Con(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bankSystem","root","wweimmortals7");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
